0f96906de72e55ccb5052173178ad02f9c49f596
Bruno Lima <brunojet@gmail.com>
2025-04-29 20:07:28 -0300

    Test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,6 @@
 import { Routes } from '@angular/router';
-import { NoticeComponent } from './pages/notice/notice.component';
-import { LoginComponent } from './pages/login/login.component';
-import { TermsComponent } from './pages/terms/terms.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'notice', pathMatch: 'full' },
-  { path: 'notice', component: NoticeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'terms', component: TermsComponent },
+  { path: '', component: AppComponent },
 ];
